a123cd97fcd6c8466d48e37e583f2104639629b1
Ali Salah <ali@salah.io>
2021-12-02 17:09:24 +0200

    init repo

---
+++ dist/build/webpack/config/blocks/css/plugins.d.ts
@@ -0,0 +1,1 @@
+export declare function getPostCssPlugins(dir: string, supportedBrowsers: string[] | undefined, defaults?: boolean, disablePostcssPresetEnv?: boolean): Promise<import('postcss').AcceptedPlugin[]>;
